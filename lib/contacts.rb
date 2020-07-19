07951d738ef1b17cfe9c15c81b8ca43c43d8417f
Samara Grant <samara.amanda01@gmail.com>
2020-07-19 02:01:03 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,8 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"][:favorite_ice_cream_flavors][0].shift
+  contacts.each do |person, contact_details_hash|
+    binding.pry
 
 
 end
